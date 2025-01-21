1b0d41a1d0a2806bde6643a528e63dc6179778f0
Catifundo <lindomarbatistao@gmail.com>
2025-01-21 15:42:36 -0300

    21-01-2025 - 2

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import listar_professores
+from .views import listar_professores, ProfessoresView
 
 urlpatterns = [
     path('professores', listar_professores),
+    path('prof', ProfessoresView.as_view())
 ]
 
 
--- api/views.py
+++ api/views.py
@@ -3,10 +3,20 @@ from .models import Cadastro
 from .serializer import CadastroSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.generics import ListCreateAPIView
 
 @api_view(['GET', 'POST'])
 def listar_professores(request):
     if request.method == 'GET':
         queryset = Cadastro.objects.all()
         serializer = CadastroSerializer(queryset, many=True)
-    return Response(serializer.data)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = CadastroSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, )
+
+class ProfessoresView(ListCreateAPIView):
+    queryset = Cadastro.objects.all()
+    serializer_class = CadastroSerializer
